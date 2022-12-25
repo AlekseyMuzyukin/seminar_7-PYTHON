b1e7647fca5b937f7205b5d4a7fe1967ebe07b3c
Aleksey <J2k_pot@mail.ru>
2022-12-25 14:19:50 +0300

    new phone_

--- Task 7/main.py
+++ Task 7/main.py
@@ -1,10 +1,10 @@
-import data_generation as dg
+# import data_generation as dg
 import user_interface as ui
 import logger as lg
 import crud
 
 
-dg.start()
+# dg.start()
 lg.logging.info('Start')
 crud.init_data_base('base_phone.csv')
 ui.ls_menu()
